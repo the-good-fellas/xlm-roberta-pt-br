b1a60242622a8afdcad582d3c5eefe9a7856c010
Rafael Morais <rdemorais.freitas@gmail.com>
2022-10-10 14:56:13 -0300

    script para envio do brwac para HF - FullSentences

--- tgfmlm/brwac_to_hf.py
+++ tgfmlm/brwac_to_hf.py
@@ -9,6 +9,7 @@ def brwac_parser(args):
   sent_tokens = []
   api = HfApi()
   msg = Printer()
+  doc_count = 0
   train_file = open(f'{args.assets_path}/brwac_train_{sent_flush_iter}.txt', 'a')
 
   train_file.write('text\n')
@@ -20,41 +21,41 @@ def brwac_parser(args):
         line.startswith('</p>') or \
         line.startswith('<g/>') or \
         line.startswith('</doc>') or \
+        line.startswith('<s>') or \
+        line.startswith('</s>') or \
         line.startswith('\n\n'):
         continue
 
       if line.startswith('<doc'):
-        if len(sents) >= 100_000:
-
-          for s in sents:
-            train_file.write(f'{s}\n')
-
-          file_name = os.path.basename(train_file.name)
-
-          msg.info(f'uploading {file_name}...')
-          train_file.flush()
-          api.upload_file(
-            path_or_fileobj=train_file.name,
-            path_in_repo=file_name,
-            repo_type='dataset',
-            repo_id=args.repo_id,
-            token=args.hf_token
-          )
-
-          os.remove(train_file.name)
-
-          sent_flush_iter += 1
-          train_file = open(f'{args.assets_path}/brwac_train_{sent_flush_iter}.txt', 'a')
-          msg.info(f'new flush idx: {idx}. file id: {sent_flush_iter}')
-          sents = []
-
-      if line.startswith('<s>'):
-        sent_tokens = []
+        doc_count += 1
         continue
 
-      if line.startswith('</s>'):
-        sent = ' '.join(sent_tokens)
-        sents.append(sent.lower())
-        continue
+      if len(sents) >= 50_000:
+
+        for s in sents:
+          train_file.write(f'{s}\n')
+
+        file_name = os.path.basename(train_file.name)
+
+        msg.info(f'uploading {file_name}...')
+        train_file.flush()
+        api.upload_file(
+          path_or_fileobj=train_file.name,
+          path_in_repo=file_name,
+          repo_type='dataset',
+          repo_id=args.repo_id,
+          token=args.hf_token
+        )
+
+        os.remove(train_file.name)
+
+        sent_flush_iter += 1
+        train_file = open(f'{args.assets_path}/brwac_train_{sent_flush_iter}.txt', 'a')
+        msg.info(f'new push. lines: {idx}. file id: {sent_flush_iter}. doc count: {doc_count}')
+        sents = []
 
       sent_tokens.append(line.replace('\n', ''))
+      if len(sent_tokens) == 510:
+        sent = ' '.join(sent_tokens)
+        sents.append(sent)
+        sent_tokens = []
